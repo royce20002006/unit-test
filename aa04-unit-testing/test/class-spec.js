62b8f20922380ca91dbaf42d68106723022bf4a0
KevinFugate <kevin.l.fugate@gmail.com>
2024-01-17 21:44:01 -0800

    crushing it dude

--- aa04-unit-testing/test/class-spec.js
+++ aa04-unit-testing/test/class-spec.js
@@ -5,30 +5,36 @@ const { Word } = require("../class");
 
 describe("Word", function () {
   describe("Word constructor function", function () {
+    let word = new Word('hello');
     it('should have a "word" property', function () {
-      expect.fail("replace with your code");
+      expect(word).to.have.property('word');
     });
-  
+
     it('should set the "word" property when a new word is created', function () {
-      expect.fail("replace with your code");
+      expect(word.word);
     });
   });
 
   describe("removeVowels function", function () {
+    let word = new Word('hello')
+    let vowels = 'aeiou'
     it("should return a the word with all vowels removed", function () {
-      expect.fail("replace with your code");
+      expect(word.word).to.not.include(vowels);
     });
   });
 
   describe("removeConsonants function", function () {
+    let word = new Word('hello')
+    let vowels = 'aeiou'
     it("should return the word with the consonants removed", function () {
-      expect.fail("replace with your code");
+      expect(word.word).to.not.include(!vowels);
     });
   });
-  
+
   describe("pigLatin function", function () {
+    let word = new Word('hello')
     it("should return the word converted to pig latin", function () {
-      expect.fail("replace with your code");
+      expect(word.pigLatin(word.word)).to.deep.equal('ellohay');
     });
   });
 });
