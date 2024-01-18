bf84209c2a7b469a01a3e7c5df48be6d87601a3a
KevinFugate <kevin.l.fugate@gmail.com>
2024-01-17 21:08:01 -0800

    funcs done dude

--- aa04-unit-testing/funcs.js
+++ aa04-unit-testing/funcs.js
@@ -1,13 +1,24 @@
 function isFive(num) {
-  // Your code here 
+  if (num === 5) {
+    return true;
+  } else { return false }
 }
 
 function isOdd(number) {
-  // Your code here 
+  if (typeof number !== 'number') {
+    throw new Error();
+  }
+  if (number % 2 !== 0) {
+    return true;
+  } else { return false }
 }
 
 function myRange(min, max, step = 1) {
-  // Your code here 
+  let res = [];
+  for (let i = min; i <= max; i += step) {
+    res.push(i);
+  }
+  return res;
 }
 
 
